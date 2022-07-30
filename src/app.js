0470026261e633102166d66ebb8baab9061444aa
Bairon00 <bairon97@icloud.com>
2022-07-30 01:37:23 +0000

    generador de numero listo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#numero").innerHTML = numberRandom();
+  //escoje un numero
+  function numberRandom() {
+    let numeros = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let random = Math.floor(Math.random() * 12);
+    return (numeros = [random]);
+  }
+  //console.log("Hello Rigo from the console!");
 };
